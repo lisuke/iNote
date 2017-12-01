2da97714cd11e8aae3a4cde8da5d63ee2a936919
lisuke <1657787678@qq.com>
2017-12-01 18:21:17 +0800

    shell: add NoteCategory

--- manager.py
+++ manager.py
@@ -2,13 +2,14 @@
 import os
 from flask_script import Manager, Shell
 from app import create_app, db
-from app.models import User,Role
+from app.models import User,Role,NoteCategory
 
 app = create_app(os.getenv('INOTE_CONFIG') or 'lisuke-dev')
+
 manager = Manager(app)
 
 def make_shell_context():
-    return dict(app=app, db=db,User=User,Role=Role)
+    return dict(app=app, db=db,User=User,Role=Role,NoteCategory=NoteCategory)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
