9d1d7b8893c0fd76544cb6291b09279e682b76d8
lisuke <1657787678@qq.com>
2017-12-31 16:13:49 +0800

    backend: add note view

---
+++ app/inote/utils/iNoteNoteUtils.py
@@ -0,0 +1,155 @@
+from flask_login import current_user
+from flask import jsonify,request,abort
+from app.models import User,NoteCategory,db,Note,Tag
+from flask_babel import gettext as _
+from datetime import datetime
+
+class iNoteNoteUtil:
+
+
+    @staticmethod
+    def post():
+        ''' post format
+        request:
+        {
+            int(cate_id):description(''),
+            str(note_title):description('')
+            str(note_content):description('')
+            list(note_tags):description('dict("id","name")')
+            str(note_editor)
+        }
+        response:
+        {
+            id:int()
+        }
+        '''
+        json = request.get_json()
+        cate = NoteCategory.query.filter_by(id=int(json['cate_id'])).first()
+
+        note = Note(name=json['new_cate_title'],content=json['note_content'],title=json['note_title'],edit_type=json["note_editor"],last_modify_datetime=datetime.utcnow())
+        note.category = cate
+        note.user = current_user
+
+        db.session.add(note)
+        db.session.commit()
+        return jsonify({'note_id':note.id})
+
+    @staticmethod
+    def delete():
+        pass
+
+    @staticmethod
+    def note2json(note):
+        json = {'note_id':note.id,'note_title':note.title,'note_content':note.content,'note_createtime':note.create_datetime,'note_modifytime':note.last_modify_datetime}
+        tags = note.tags.all()
+        tags_json = []
+        for tag in tags:
+            tags_json.append({'tag_id':tag.id,'tag_name':tag.name})
+        json['tags'] = tags_json
+        return json
+
+    @staticmethod
+    def get():
+        id = int(request.args.get('note_id'))
+
+        note = Note.query.filter_by(id=id).first()
+        if current_user.id == note.user.id:
+            ret_json = iNoteNoteUtil.note2json(note)
+            return jsonify(ret_json)
+        else:
+            return jsonify([{'status':'permission denied'}])
+
+    @staticmethod
+    def put():
+        json = request.get_json()
+
+        if json['type'] == 'modify title':
+            note = Note.query.filter_by(id=int(json['note_id'])).first()
+            if note is None:
+                return jsonify({'status': 'resource not found'})
+            if note.user_id != current_user.id:
+                return jsonify({'status':'permission denied'})
+            else:
+                note.name = json['new_note_title']
+                db.session.commit()
+                return jsonify({'status':'success'})
+
+        if json['type'] == 'modify content':
+            note = Note.query.filter_by(id=int(json['note_id'])).first()
+            if note is None:
+                return jsonify({'status': 'resource not found'})
+            if note.user_id != current_user.id:
+                return jsonify({'status':'permission denied'})
+            else:
+                note.name = json['new_note_content']
+                db.session.commit()
+                return jsonify({'status':'success'})
+
+        if json['type'] == 'modify tags':
+            note = Note.query.filter_by(id=int(json['note_id'])).first()
+            json_tags = json['tags']
+
+            if note is None:
+                return jsonify({'status': 'resource not found'})
+            if note.user_id != current_user.id:
+                return jsonify({'status':'permission denied'})
+            else:
+                for json_tag in json_tags:
+                    t_name = json_tag.tag_name
+                    tag = Tag.query.filter_by(name=t_name).first()
+                    if tag is not None:
+                        note.tags.append(tag)
+                    else:
+                        tag = Tag(name=t_name)
+                        db.session.add(tag)
+                        current_user.tags.append(tag)
+                        note.tags.append(tag)
+                db.session.commit()
+                return jsonify({'status':'success'})
+
+        if json['type'] == 'del tag':
+            note = Note.query.filter_by(id=int(json['note_id'])).first()
+            tag = Tag.query.filter_by(id=int(json['tag_id'])).first()
+            if note is None or tag is None:
+                return jsonify({'status': 'resource not found'})
+            if note.user_id != current_user.id:
+                return jsonify({'status':'permission denied'})
+            else:
+                note.tags.remove(tag)
+                db.session.commit()
+                return jsonify({'status':'success'})
+
+        if json['type'] == 'query tags':
+            note = Note.query.filter_by(id=int(json['note_id'])).first()
+            if note is None:
+                return jsonify({'status': 'resource not found'})
+            if note.user_id != current_user.id:
+                return jsonify({'status':'permission denied'})
+            else:
+                tags = note.tags.all()
+                tags_json = []
+                for tag in tags:
+                    tags_json.append({'tag_id': tag.id, 'tag_name': tag.name})
+                return jsonify(tags_json)
+
+        if json['type'] == 'move to':
+            cate_id = int(json['cate_id'])
+            note_id = int(json['note_id'])
+            current_cate = NoteCategory.query.filter_by(id=cate_id).first()
+            note = Note.query.filter_by(id=note_id).first()
+
+            if current_cate is None or (note is None ):
+                #未找到节点
+                return jsonify({'status': 'resource not found'})
+            if current_cate.user_id != current_user.id or ( note.user_id != current_user.id):
+                #跨用户操作
+                return jsonify({'status':'permission denied'})
+
+            if  note.category == current_cate:
+                #已就位
+                return jsonify({'status': 'already on this pos'})
+
+            note.category = current_cate
+            db.session.commit()
+            return jsonify({'status':'success'})
+
--- app/inote/views.py
+++ app/inote/views.py
@@ -2,7 +2,6 @@ from . import inote
 from flask import render_template,redirect,url_for,request,jsonify
 from app.auth.views import login_required
 from flask_login import current_user
-from .utils.iNoteCategoryUtils import iNoteCategoryUtil
 
 @inote.route('/',methods=['GET','POST'])
 @inote.route('/index',methods=['GET','POST'])
@@ -11,6 +10,9 @@ def index():
     print(request.get_json())
     return render_template('inote/inote.html')
 
+
+
+from .utils.iNoteCategoryUtils import iNoteCategoryUtil
 
 @inote.route('/category',methods=['GET','POST','PUT','DELETE'])
 @login_required
@@ -37,3 +39,39 @@ def category():
         #DELETE,delete data
         return iNoteCategoryUtil.delete()
 
+
+from .utils.iNoteNoteUtils import iNoteNoteUtil
+
+@inote.route('/note',methods=['GET','POST','PUT','DELETE'])
+@login_required
+def note():
+
+    if request.method == 'GET':
+        # GET, get data
+        return iNoteNoteUtil.get()
+
+    elif request.method == 'POST':
+        #POST, add data
+        return iNoteNoteUtil.post()
+
+    elif request.method == 'PUT':
+        #PUT, update data
+        return iNoteNoteUtil.put()
+
+    elif request.method == 'DELETE':
+        #DELETE,delete data
+        return iNoteNoteUtil.delete()
+
+from .utils.iNoteNoteListUtils import iNoteNoteListUtil
+
+@inote.route('/notelist',methods=['GET'])
+@login_required
+def note_list():
+    if request.method == 'GET':
+        return iNoteNoteListUtil.get()
+    if request.method == 'POST':
+        return iNoteNoteListUtil.post()
+    if request.method == 'PUT':
+        return iNoteNoteListUtil.put()
+    if request.method == 'DELETE':
+        return iNoteNoteListUtil.delete()
