ee04c3f25a03cc3ecd47ffee5845cc64182d02c8
lisuke <1657787678@qq.com>
2018-01-07 15:10:54 +0800

    frontend: INote_list.class has changed.

--- app/static/js/inote-list.js
+++ app/static/js/inote-list.js
@@ -1,10 +1,11 @@
-function INote_list(pos_id,category_id){
+
+function INote_list(pos_id,CateId){
     var list_name = "note-list";
     var key = pos_id;
-    var id = category_id;
+    var cate_id = CateId;
 
-    var addItem = function(id,title,content,img){
-        var newItem = $('<div id="note-item " class="note-items item" inote-id='+ id +'> </div>');
+    var addItem = function(id,title,note_edit_type,content,img){
+        var newItem = $('<div id="note-item " class="note-items item" inote-id='+ cate_id +' inote_edit_type='+note_edit_type+'> </div>');
         newItem.append('<span class="item-title">' + title + '</span>');
         newItem.append('<div class="item-body" ><span class="item-content-view">' + content + '</span></div>');
         if(img)
@@ -14,51 +15,42 @@ function INote_list(pos_id,category_id){
             $(this).addClass("item-active");
             $(this).siblings().removeClass("item-active");
             console.log($(this).attr('inote-id'));
-
-            resetEditor();
+            console.log($(this).attr('inote_edit_type'));
+            resetEditor($(this).attr('inote-id'),$(this).attr('inote_edit_type'));
         });
     };
+
     var get_list = function(){
         $.ajax({
             type:'get',
             url:"notelist",
             data:{
-                'cate_id':-1,
+                'cate_id':cate_id,
             },
             dataType: "json",
             contentType: "application/json",
             success:function(data){
-                console.log('notelist:',data)
+                console.log('notelist: ',data)
                 for (var i=0;i<data.length;i++)
                 {
-                    addItem(data[i].note_id,data[i].note_title,data[i].note_content);
+                    addItem(data[i].note_id,data[i].note_title,data[i].note_edit_type,data[i].note_content);
                 }
             }
         });
-
     };
+
     var init = function(){
         $("#" + list_name).remove();
         $(key).append('<div id="note-list" class="note-list">');
 //        this.addItem(0,"test","hello world"," https://leanote.com/api/file/getImage?fileId=59d5102bab644101ff0021ab");
-//        this.addItem(1,"test","hello world","");
-//        this.addItem(2,"test","hello world");
+
         get_list();
         return this;
     };
-    var resetEditor = function(){
-        $("#note").remove();
 
-        var note = $('<div id="note"></div>');
-        note.append('<div id="note-title"><input name="noteTitle" id="noteTitle" value placeholder="无标题" tabindex="1" style="width:100%;"></div>');
-        note.append('<div id="note-tag"><input name="noteTag"></div>')
-
-        $("#content").append(note);
-
-        note.append('<div id="note-editor"><textarea id="ckeditor" tabindex="2">test</textarea></div>');
-        note.append('<div id="note-status-bar"></div>');
-        ReLoadCkeditor();
-
+    var resetEditor = function(note_id, editor_type){
+        note = new Note(note_id, cate_id , editor_type);
+        note.init();
     };
 
     return {
