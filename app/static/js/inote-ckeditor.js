d2d18680c9550b3774204c395158fd7fa24eaf41
lisuke <1657787678@qq.com>
2018-01-10 15:33:01 +0800

    frontend: add ckeditor->iframe->contentDocument->body keydown save event

--- app/static/js/inote-ckeditor.js
+++ app/static/js/inote-ckeditor.js
@@ -1,7 +1,6 @@
 
-    function ReLoadCkeditor()
+    function ReLoadCkeditor(save)
     {
-
         var ck_editor = CKEDITOR.replace('ckeditor');
         ck_editor.on('instanceReady',function(event)                  //准备
         {
@@ -20,37 +19,59 @@
                         var height = $(window).height()- $("#"+che_find+"_top").position().top - $("#"+che_find+"_top").outerHeight(true) - $("#"+che_find+"_bottom").outerHeight(true) - 2 ;
                         $('#'+che_find+'_contents').height(height);
                    }
+
+                    document.getElementsByTagName('iframe')[0].contentDocument.getElementsByTagName('body')[0].onkeydown=function(e){
+                        if (e.ctrlKey && e.key=='s')
+                        {// Ctrl + S
+                            save();
+
+                            e.returnValue=false;
+                        }
+
+                    };
                 },0);
         },null,null,9999);
         $(window).resize(function () {
             var che_find = $("#cke_ckeditor")[0].classList[0];
             var height = $(window).height()- $("#"+che_find+"_top").position().top - $("#"+che_find+"_top").outerHeight(true) - $("#"+che_find+"_bottom").outerHeight(true) - 2;
             $('#'+che_find+'_contents').height(height);
+
+
         });
+        return ck_editor;
     }
-    $('#ckeditor').ready(ReLoadCkeditor);
+//    $('#ckeditor').ready(ReLoadCkeditor);
 
     var Inote_CKEditor = function(name){
+        var editor = null;
+        var keydown = null;
         if(!name)
             this.editor_name =  "ckeditor";
         else
             this.editor_name = name;
-        console.log(this.editor_name)
+//        console.log(this.editor_name)
 
         this.construct=function(){
-            ReLoadCkeditor();
+            editor = ReLoadCkeditor(keydown);
         }
         this.destruct=function(){
             console.log("test");
         }
         this.getData=function(){
-            return "test";
+            return editor.getData();
         }
         this.setData=function(data){
-            console.log('set: ',data);
+            var __editor = editor;
+            setTimeout(function()
+                {     //延时加载
+                    __editor.setData(data);
+                },null,null,10);
         }
         this.getHTML=function(){
             return '<div id="note-editor"><textarea id="ckeditor" tabindex="2"></textarea></div>';
+        }
+        this.setKeyEvent=function(callback){
+            keydown = callback;
         }
 
     };
