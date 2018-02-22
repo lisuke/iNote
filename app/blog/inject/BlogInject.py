c600c84b21583ff4e27eb0ee110e9b5dd5ee7790
lisuke <1657787678@qq.com>
2018-02-22 16:09:26 +0800

    modify BlogInject.py

--- app/blog/inject/BlogInject.py
+++ app/blog/inject/BlogInject.py
@@ -3,22 +3,13 @@ from flask import request
 
 blogIndex = INoteInject()
 
+class userInfo(InjectContext):
+    def __enter__(self):
+        print('construct userInfo')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('destruct userInfo')
 
 @blogIndex.inject_ctx_class
 def ctx():
-    class userInfo(InjectContext):
-        def __enter__(self):
-            print('test')
-
-        def __exit__(self, exc_type, exc_val, exc_tb):
-            print('test')
-
     return userInfo
-
-
-@blogIndex.use_ctx
-def use():
-    pass
-
-
-use()
