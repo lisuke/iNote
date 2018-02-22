472134c2d8ba2ec9ed79e1dc50f9992bbc74d5ca
lisuke <1657787678@qq.com>
2018-02-22 16:10:54 +0800

    module blog: fix inject

--- app/blog/__init__.py
+++ app/blog/__init__.py
@@ -1,8 +1,6 @@
 from flask import Blueprint
+from .inject import BlogInject
 
-blog = Blueprint('blog', __name__, static_folder='static', template_folder='templates')
+blog = Blueprint('blog', __name__, static_folder='static',
+                 template_folder='templates')
 
-from . import views
-# from .inject.BlogInject import
-
-# iNoteBlogContext.ctx_func_init()
